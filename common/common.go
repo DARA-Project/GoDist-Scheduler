b1e1d76318463a143b1dc00b43910f3476073aa5
Wanton Solutions <sgrant09@cs.ubc.ca>
2018-06-15 16:26:38 -0700

    fixed merge bugs, perhaps rebase the prior commit :(

--- common/common.go
+++ common/common.go
@@ -63,27 +63,64 @@ const (
 	MAXGOROUTINES = 4096
 )
 
-/**
-  * Struct representing a Process/Node in the system
-  */
+//DaraProc is used to communicate control and data information between
+//a single instrumented go runtime and the global scheduler. One of
+//these strucures is mapped into shared memory for each process that
+//is launched during an execution. If there are more runtimes active
+//Than DaraProcs the additional runtimes will not be controlled by the
+//global scheduler, or Segfault immidiatly 
 type DaraProc struct {
+
+	//Lock is used to controll the execution of a process. A process
+	//which is running but Not scheduled will spin on this lock using
+	//checkandset operations If the lock is held The owner can modify
+	//the state of the DaraProc
 	Lock int32
+	
+	//Run is a depricated var with multipul purposes. Procs set their
+	//Run to -1 when they Are done running (in replay mode) to let the
+	//scheduler know they are done. The global scheduler sets this
+	//value to 2 to let the runtime know its replay, and 3 for record
+	//1 is used to denote the first event, and 0 indicates this
+	//variable has not been initalized Originally Run was intended to
+	//report the id of the goroutine that was executed, but that was
+	//not always the same so the program counter  was needed, now
+	//RunningRoutine is used to report this
 	Run int
-	TotalRoutines int
+	//RunningRoutine is the goroutine scheduled, running, or ran, for
+	//any single replayed event in a schedule. In Record, the
+	//executed goroutine is reported via this variable, in Replay the
+	//global scheduler tells the runtime which routine to run with
+	//RunningRoutine
 	RunningRoutine RoutineInfo
+	//Routines is the full set of goroutines a process is allowed to
+	//run. The total number is allocated upfront so that shared memory
+	//does not need to be resized dynamically. After each itteration
+	//of scheduling runtimes update the states of all their routines
+	//via this structure
 	Routines [MAXGOROUTINES]RoutineInfo
 }
 
-/**
-  * Struct representing all the information for a goroutine
-  */
+//RoutineInfo contains data specific to a single goroutine
 type RoutineInfo struct {
+	//Set to one of the statuses in the constant block above
 	Status uint32
+	//Goroutine id as set by the runtime. This is sometimes usefull
+	//for detecting which routine is which, but it is not always the
+	//same between runs. However 1 is allways main, while 2 is a
+	//finalizer, and 3 is a garbage collection invocator.
 	Gid int
+	//Program counter that this goroutine was launched from
 	Gpc uintptr
+	//A count of how many other goroutines were launched on the same
+	//pc prior to this goroutine. (Gpc,Routinecount) is a unique id
+	//for a goroutine on a given processor.
 	RoutineCount int
+	//A textual discription of the function this goroutine was forked
+	//from.In the future it can be removed.
 	FuncInfo [64]byte
 }
+
 
 func (ri *RoutineInfo) String() string {
 	return fmt.Sprintf("[Status: %s Gid: %d Gpc: %d Rc: %d F: %s]",gStatusStrings[(*ri).Status],(*ri).Gid,(*ri).Gpc,(*ri).RoutineCount, string((*ri).FuncInfo[:64]))
--- scheduler.go
+++ scheduler.go
@@ -21,6 +21,7 @@ var (
 	procs = flag.Int("procs", 1, "The number of processes to model check")
 	record = flag.Bool("w", false, "Record an execution")
 	replay = flag.Bool("r", false, "Replay an execution")
+	explore = flag.Bool("e", false, "Explore a recorded execution")
 	manual = flag.Bool("m", false, "Manually step through an execution using a udp connection into the scheduler")
 )
 
@@ -82,100 +83,10 @@ const(
 	RECORDLEN = 1000
 )
 
-//Goroutine states from runtime/proc.go
-const (
-	_Gidle = iota 		// 0
-	_Grunnable 			// 1
-	_Grunning 			// 2
-	_Gsyscall 			// 3
-	_Gwaiting 			// 4
-	_Gmoribund_unused 	// 5
-	_Gdead 				// 6
-	_Genqueue_unused 	// 7
-	_Gcopystack 		// 8
-	_Gscan         = 0x1000
-	_Gscanrunnable = _Gscan + _Grunnable // 0x1001
-	_Gscanrunning  = _Gscan + _Grunning  // 0x1002
-	_Gscansyscall  = _Gscan + _Gsyscall  // 0x1003
-	_Gscanwaiting  = _Gscan + _Gwaiting  // 0x1004
-)
-
-//array of status to string from runtime/proc.go
-var gStatusStrings = [...]string{
-	_Gidle:      "idle",
-	_Grunnable:  "runnable",
-	_Grunning:   "running",
-	_Gsyscall:   "syscall",
-	_Gwaiting:   "waiting",
-	_Gdead:      "dead",
-	_Gcopystack: "copystack",
-}
-
-//DaraProc is used to communicate control and data information between
-//a single instrumented go runtime and the global scheduler. One of
-//these strucures is mapped into shared memory for each process that
-//is launched during an execution. If there are more runtimes active
-//Than DaraProcs the additional runtimes will not be controlled by the
-//global scheduler, or Segfault immidiatly 
-type DaraProc struct {
-
-	//Lock is used to controll the execution of a process. A process
-	//which is running but Not scheduled will spin on this lock using
-	//checkandset operations If the lock is held The owner can modify
-	//the state of the DaraProc
-	Lock int32
-	
-	//Run is a depricated var with multipul purposes. Procs set their
-	//Run to -1 when they Are done running (in replay mode) to let the
-	//scheduler know they are done. The global scheduler sets this
-	//value to 2 to let the runtime know its replay, and 3 for record
-	//1 is used to denote the first event, and 0 indicates this
-	//variable has not been initalized Originally Run was intended to
-	//report the id of the goroutine that was executed, but that was
-	//not always the same so the program counter  was needed, now
-	//RunningRoutine is used to report this
-	Run int
-	//RunningRoutine is the goroutine scheduled, running, or ran, for
-	//any single replayed event in a schedule. In Record, the
-	//executed goroutine is reported via this variable, in Replay the
-	//global scheduler tells the runtime which routine to run with
-	//RunningRoutine
-	RunningRoutine RoutineInfo
-	//Routines is the full set of goroutines a process is allowed to
-	//run. The total number is allocated upfront so that shared memory
-	//does not need to be resized dynamically. After each itteration
-	//of scheduling runtimes update the states of all their routines
-	//via this structure
-	Routines [MAXGOROUTINES]RoutineInfo
-}
-
-//RoutineInfo contains data specific to a single goroutine
-type RoutineInfo struct {
-	//Set to one of the statuses in the constant block above
-	Status uint32
-	//Goroutine id as set by the runtime. This is sometimes usefull
-	//for detecting which routine is which, but it is not always the
-	//same between runs. However 1 is allways main, while 2 is a
-	//finalizer, and 3 is a garbage collection invocator.
-	Gid int
-	//Program counter that this goroutine was launched from
-	Gpc uintptr
-	//A count of how many other goroutines were launched on the same
-	//pc prior to this goroutine. (Gpc,Routinecount) is a unique id
-	//for a goroutine on a given processor.
-	RoutineCount int
-	//A textual discription of the function this goroutine was forked
-	//from.In the future it can be removed.
-	FuncInfo [64]byte
-}
-
-func (ri *RoutineInfo) String() string {
-	return fmt.Sprintf("[Status: %s Gid: %d Gpc: %d Rc: %d F: %s]",gStatusStrings[(*ri).Status],(*ri).Gid,(*ri).Gpc,(*ri).RoutineCount, string((*ri).FuncInfo[:64]))
-}
-
 //*****************************************************************/
 // 					END SHARED VARIABLES
 //*****************************************************************/
+
 
 //Global variables specific to shared memory
 var (
@@ -185,7 +96,7 @@ var (
 	//where there is no error type set
 	err int
 	//Procchan is the communication channel laid over shared memory
-	procchan *[CHANNELS]DaraProc
+	procchan *[CHANNELS]common.DaraProc
 	//Used to determine which process (runtime) will be run on the
 	//next scheduling decision when in record mode
 	LastProc int
@@ -201,38 +112,7 @@ var (
 	//TODO allow users to specifcy scheduling decisions based on udp
 	//stepping
 	udpb []byte
-	//global schedule
-	//TODO make all schedule operations object calls on the scedule
-	//(ie schedule.next())
-	schedule Schedule
 )
-
-//Event is a wrapper for a single scheduling event
-type Event struct {
-	//ProcID is an ID for a single runtime
-	ProcID int
-	//Routine contians information about the routine to run, including
-	//state, blocking, and id information
-	Routine RoutineInfo
-}
-
-func (e *Event) String() string {
-		return fmt.Sprintf("[ProcID %d, %s]\n",(*e).ProcID,(*e).Routine.String())
-}
-
-//Type which encapsulates a single schedule
-//TODO integerate with vaastav to build a single schedule for DPOR
-type Schedule []Event
-
-//Get Schedule string
-//TODO make this fast with buffers
-func (s *Schedule) String() string{
-	var output string
-	for i := range *s {
-		output += (*s)[i].String()
-	}
-	return output
-}
 
 func checkargs() {
 	flag.Parse()
@@ -262,6 +142,9 @@ func forward() {
 	}
 }
 
+	//global schedule
+	//TODO make all schedule operations object calls on the scedule
+	//(ie schedule.next())
 var schedule common.Schedule
 
 func roundRobin() int {
