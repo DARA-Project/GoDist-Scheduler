76d8af788d3fbe404ef9206b9da5b8af30f89298
vaastav <vaastav.anand05@gmail.com>
2018-06-20 13:46:11 -0700

    Update file read example with reading 2 different files

--- examples/SimpleFileRead/file_read.go
+++ examples/SimpleFileRead/file_read.go
@@ -4,7 +4,30 @@ import (
 	"fmt"
 	"log"
 	"os"
+//	"time"
 )
+
+func bar(c chan int) {
+	for i := 0; i < 5; i++{
+		fmt.Println("Opening file2 now")
+		f, err := os.Open("file2.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Reading file2 now")
+		b1 := make([]byte, 20)
+		n1, err := f.Read(b1)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%d bytes of file2: %s\n", n1, string(b1[:n1]))
+		f.Close()
+//		time.Sleep(time.Millisecond)
+	}
+	c <- 1
+
+}
 
 func foo(c chan int) {
 	for i := 0; i < 5; i++{
@@ -22,13 +45,18 @@ func foo(c chan int) {
 		}
 		fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
 		f.Close()
+//		time.Sleep(time.Millisecond)
 	}
 	c <- 1
 }
 
 func main() {
 	c := make(chan int)
+	c2 := make(chan int)
 	go foo(c)
+	go bar(c2)
 	x := <-c
+	y := <-c2
 	log.Println(x)
+	log.Println(y)
 }
