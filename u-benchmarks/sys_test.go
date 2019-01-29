ec27dfebb114464e9ec57075bfd452d0be3c5e1f
Daniel Du <ddu1997@gmail.com>
2019-01-29 15:22:24 -0800

    Add helpers and clean up existing benchmarks

--- u-benchmarks/sys_test.go
+++ u-benchmarks/sys_test.go
@@ -7,49 +7,72 @@ import (
 )
 
 func BenchmarkFileOpen(b *testing.B) {
+	f := CreateOrDie("hello_world.txt")
+	f.Close()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		os.Open("hello_world.txt")
 	}
+	b.StopTimer()
+	RemoveOrDie("hello_world.txt")
+
 }
 
 func BenchmarkFileRead(b *testing.B) {
-	f, err := os.Open("hello_world.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	f := CreateOrDie("hello_world.txt")
 	b1 := make([]byte, 20)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		f.Read(b1)
 	}
+	b.StopTimer()
+	f.Close()
+	RemoveOrDie("hello_world.txt")
 }
 
 func BenchmarkFileClose(b *testing.B) {
-	f, err := os.Open("hello_world.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	f := CreateOrDie("hello_world.txt")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		f.Close()
 		b.StopTimer()
-		f, _ = os.Open("hello_world.txt")
+		f = OpenOrDie("hello_world.txt")
 		b.StartTimer()
 	}
+	RemoveOrDie("hello_world.txt")
 }
 
 func BenchmarkFileWrite(b *testing.B) {
-	f, err := os.Create("write_hello.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	f := CreateOrDie("write_hello.txt")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, err = f.Write([]byte("Hello World\n"))
+		f.Write([]byte("Hello World\n"))
 	}
 	b.StopTimer()
 	f.Close()
-	err = os.Remove("write_hello.txt")
+	RemoveOrDie("write_hello.txt")
+}
+
+// Helpers to reduce boilerplate code
+
+func CreateOrDie(name string) *os.File {
+	f, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func OpenOrDie(name string) *os.File {
+	f, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func RemoveOrDie(name string) {
+	err := os.Remove(name)
 	if err != nil {
 		log.Fatal(err)
 	}
