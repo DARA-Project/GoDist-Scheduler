81d2cb5a6c7835dd0f35ff86ca7e9bb0dd43ef9b
Vaastav Anand <vaastav.anand05@gmail.com>
2020-06-02 00:32:05 -0700

    Remove DInv dependency. Sorry Stew!

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,5 @@
 
 # For converting properties into executable functions
 go get github.com/novalagung/go-eek
-# Dinv for now.....But it shall be gone as soon as instrumentation is updated
-go get bitbucket.org/bestchai/dinv/capture
 # Delve for finding the full variables names in a given binary
 go get github.com/go-delve/delve/pkg/proc
