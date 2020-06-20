4f6a76d877e28783cb4f6dab1a196e02afb4ab69
mayanktiwari09 <mayanktiwari09@gmail.com>
2020-06-20 05:39:14 +0000

    Implementing PR comments

--- examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go
+++ examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/DARA-Project/GoDist-Scheduler/propchecker"
+//	"github.com/DARA-Project/GoDist-Scheduler/propchecker"
 	"log"
 	"os"
 	"runtime"
@@ -16,19 +16,16 @@ type Consumer struct {
 
 // NewConsumer creates a Consumer
 func NewConsumer(msgs *chan int) *Consumer {
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:18:21")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:18:103")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:19:21")
 	return &Consumer{msgs: msgs}
 }
 
 // consume reads the msgs channel
 func (c *Consumer) consume() {
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:24:27")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:24:103")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:25:27")
 	fmt.Println("[consume]: Started")
 	for {
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:27:31")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:27:103")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:28:103")
 		msg := <-*c.msgs
 		fmt.Println("[consume]: Received:", msg)
 	}
@@ -42,38 +39,32 @@ type Producer struct {
 
 // NewProducer creates a Producer
 func NewProducer(msgs *chan int, done *chan bool) *Producer {
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:41:44")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:41:103")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:42:103")
 	return &Producer{msgs: msgs, done: done}
 }
 
 // produce creates and sends the message through msgs channel
 func (p *Producer) produce(max int) {
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:47:50")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:47:103")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:48:103")
 	fmt.Println("[produce]: Started")
 	for i := 0; i < max; i++ {
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:50:54")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:50:103")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:51:103")
 		fmt.Println("[produce]: Sending ", i)
 		*p.msgs <- i
 	}
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:55:57")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:54:55")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:55:55")
 	*p.done <- true
 	fmt.Println("[produce]: Done")
 }
 
 func main() {
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:60:64")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:59:103")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:61:103")
 
-	checker, err := propchecker.NewChecker("./property/example.prop")
-	if err != nil {
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:64:66")
-		log.Fatal(err)
-	}
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:68:75")
+//	checker, err := propchecker.NewChecker("./property/example.prop")
+//	if err != nil {
+//		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:64:66")
+//		log.Fatal(err)
+//	}
 
 	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to `file`")
 	memprofile := flag.String("memprofile", "", "write memory profile to `file`")
@@ -83,27 +74,21 @@ func main() {
 	flag.Parse()
 
 	if *cpuprofile != "" {
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:75:78")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:74:103")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:77:103")
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:78:81")
 			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:80:84")
 			log.Fatal("could not create CPU profile: ", err)
 		}
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:82:83")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:85:86")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:83:86")
 		if err := pprof.StartCPUProfile(f); err != nil {
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:83:86")
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:81:103")
+			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:85:86")
 			log.Fatal("could not start CPU profile: ", err)
 		}
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:87:88")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:84:84")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:88:84")
 		defer pprof.StopCPUProfile()
 	}
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:90:115")
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:94:105")
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:91:105")
 
 	var msgs = make(chan int)	// channel to send messages
 	var done = make(chan bool)	// channel to control when production is done
@@ -112,45 +97,43 @@ func main() {
 
 	go NewConsumer(&msgs).consume()
 
-	fmt.Println(" ", <-done)
+	<-done
 
-	sendings := runtime.NumSendings(msgs)
-	deliveries := runtime.NumDeliveries(msgs)
-	fmt.Println("[main]: Channel sendings = ", sendings)
-	fmt.Println("[main]: Channel deliveries = ", deliveries)
+	runtime.DaraLog("LeftElements", "main.leftElements", runtime.NumSendings(msgs) - runtime.NumDeliveries(msgs))
 
-	context := make(map[string]interface{})
-	context["main.a"] = sendings
-	context["main.b"] = deliveries
+//	sendings := runtime.NumSendings(msgs)
+//	deliveries := runtime.NumDeliveries(msgs)
+//	fmt.Println("[main]: Channel sendings = ", sendings)
+//	fmt.Println("[main]: Channel deliveries = ", deliveries)
 
-	result, failures, err := checker.Check(context)
-	log.Println("All properties passed:", result)
-	log.Println("Total property check failures:", len(*failures))
-	if err != nil {
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:115:117")
-		log.Fatal(err)
-	}
-	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:119:119")
+//	context := make(map[string]interface{})
+//	context["main.a"] = sendings
+//	context["main.b"] = deliveries
+
+//	result, failures, err := checker.Check(context)
+//	log.Println("All properties passed:", result)
+//	log.Println("Total property check failures:", len(*failures))
+
+//	if err != nil {
+//		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:118:117")
+//		log.Fatal(err)
+//	}
+	runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:121:119")
 
 	if *memprofile != "" {
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:119:122")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:100:102")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:124:102")
 		f, err := os.Create(*memprofile)
 		if err != nil {
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:122:125")
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:108:112")
+			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:127:112")
 			log.Fatal("could not create memory profile: ", err)
 		}
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:126:128")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:113:115")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:130:115")
 		runtime.GC()
 		if err := pprof.WriteHeapProfile(f); err != nil {
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:128:131")
-			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:115:119")
+			runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:133:119")
 			log.Fatal("could not write memory profile: ", err)
 		}
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:132:133")
-		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:111:111")
+		runtime.ReportBlockCoverage("../examples/ProducerConsumerInstrument/ProducerConsumerInstrument.go:136:111")
 		f.Close()
 	}
 }
